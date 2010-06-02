359316b924de35fb9ff58ce175aa3a3c5240a272
Saulo Arruda <sauloarruda@gmail.com>
2010-06-02 18:17:37 -0400

    configuração de aplicação hello world usando o plugin do mvn

---
+++ src/main/java/br/com/javamagazine/jme/MainMIDlet.java
@@ -0,0 +1,36 @@
+package br.com.javamagazine.jme;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Form;
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+public class MainMIDlet extends MIDlet {
+
+	protected void startApp() throws MIDletStateChangeException {
+		Form form = new Form("MIDlet");
+		form.addCommand(new Command("Sair", Command.EXIT, 1));
+		form.append("Hello World!");
+		form.setCommandListener(new CommandListener() {
+			public void commandAction(Command arg0, Displayable arg1) {
+				try {
+					destroyApp(false);
+				} catch (MIDletStateChangeException e) {
+					e.printStackTrace();
+				}
+				notifyDestroyed();
+			}
+		});
+		Display.getDisplay(this).setCurrent(form);
+	}
+
+	protected void destroyApp(boolean arg0) throws MIDletStateChangeException {
+	}
+
+	protected void pauseApp() {
+	}
+
+}
